be2b1904d65378e9eceadd38fc43ad4faa4f3b3d
mad <hatemodify@gmail.com>
2018-03-01 23:51:37 +0900

    update

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    body: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date
+    }
+}, {
+        toObject: {
+            virtuals: true
+        }
+    });
+
+postSchema.virtual('createdDate').get(() => {
+    return getDate(this.createdAt);
+});
+
+postSchema.virtual('createdTime').get(() => {
+    return getTime(this.createdAt);
+});
+
+postSchema.virtual('updateDate').get(() => {
+    return getDate(this.updatedAt);
+});
+
+postSchema.virtual('updateTime').get(() => {
+    return getTime(this.updatedAt);
+});
+
+const Post = mongoose.model('post', postSchema);
+
+function getDate(dateObj) {
+    if (dateObj instanceof Date)
+        return dateObj.getFullYear() + "-" + get2digits(dateObj.getMonth() + 1) + "-" + get2digits(dateObj.getDate());
+}
+
+function getTime(dateObj) {
+    if (dateObj instanceof Date)
+        return get2digits(dateObj.getHours()) + ":" + get2digits(dateObj.getMinutes()) + ":" + get2digits(dateObj.getSeconds());
+}
+
+function get2digits(num) {
+    return ("0" + num).slice(-2);
+}
+
+
+module.exports = Post;
